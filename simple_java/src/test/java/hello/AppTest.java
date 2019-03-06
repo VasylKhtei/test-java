29fc15badc0d536603e7b1707fcb96e96c959a9b
Vasyl Khtei <vasylkhtei@gmail.com>
2019-03-06 18:08:59 +0200

    Added fail test

--- simple_java/src/test/java/hello/AppTest.java
+++ simple_java/src/test/java/hello/AppTest.java
@@ -3,6 +3,7 @@ package hello;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 /**
  * Unit test for simple App.
@@ -12,9 +13,19 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
+   @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+    
+    @Test
+    public void testNothing() {
+    }
+    
+    @Test
+    public void testWillAlwaysFail() {
+        Assert.fail("An error message");
+    }
+    
 }
